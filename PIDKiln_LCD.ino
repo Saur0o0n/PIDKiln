3f8e79f44f5532471d3c84bfb2abb764815f185f
Adrian Siemieniak <sauron@ordugh.org>
2019-08-25 10:42:54 +0200

    new data files, Documentation directory

---
+++ PIDKiln_LCD.ino
@@ -0,0 +1,48 @@
+/*
+** LCD Display components
+**
+*/
+
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+//
+//Bylo U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R2, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
+//U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R2, /* clock=*/ 18, /* data=*/ 23, /* CS=*/ 5, /* reset=*/ 4);
+U8G2_ST7920_128X64_F_HW_SPI u8g2(U8G2_R2, /* CS=*/ 5, /* reset=*/ 4);
+
+//U8G2_ST7920_128X64_F_8080 u8g2(U8G2_R0, 8, 9, 10, 11, 4, 5, 6, 7, /*enable=*/ 18 /* A4 */, /*cs=*/ U8X8_PIN_NONE, /*dc/rs=*/ 17 /* A3 */, /*reset=*/ 15 /* A1 */);  // Remember to set R/W to 0 
+//U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 18 /* A4 */ , /* data=*/ 16 /* A2 */, /* CS=*/ 17 /* A3 */, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
+//U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 14, /* data=*/ 13, /* CS=*/ 15, /* reset=*/ 16); // Feather HUZZAH ESP8266, E=clock=14, RW=data=13, RS=CS
+//U8G2_ST7920_128X64_F_HW_SPI u8g2(U8G2_R0, /* CS=*/ 10, /* reset=*/ 8);
+//U8G2_ST7920_128X64_F_HW_SPI u8g2(U8G2_R0, /* CS=*/ 15, /* reset=*/ 16); // Feather HUZZAH ESP8266, E=clock=14, RW=data=13, RS=CS
+
+// Write short messages during starting
+void load_msg(char msg[20]){
+  u8g2.setDrawColor(0);
+  u8g2.drawBox(3, 31, 121, 22);
+  u8g2.setDrawColor(1);
+  u8g2.drawStr(10,45,msg);
+  u8g2.sendBuffer();
+}
+
+// Setup LCD screen
+void setup_lcd(void) {
+  
+  u8g2.begin();
+  u8g2.clearBuffer();          // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  u8g2.drawStr(28,30,pver);
+  u8g2.drawStr(38,45,"starting...");
+  u8g2.drawFrame(2,2,123,59);
+  u8g2.drawFrame(0,0,127,63);
+  u8g2.sendBuffer();          // transfer internal memory to the display
+  delay(500);
+}
